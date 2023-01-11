e402db4d0ec20042fadf1f7a3ea73ce6cdc1c0c2
gleidson <angelolustosa@gmail.com>
2023-01-11 09:59:17 -0300

    aula sobre variaveis e constantes, trabalho: fazer a calculadora

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// document.getElementById("container").innerHTML = "conteudo";
+// document.getElementById("container").innerHTML = "outra coisa";
+
+let container = document.getElementById("container");
+
+// let macas = 2;
+// let banana = 3;
+// let soma = macas + banana;
+
+// container.innerHTML = soma;
+
+// let texto = "Meu nome é ";
+// let nome = "Gleidson Teixeira";
+// let soma = texto + nome;
+
+// container.innerHTML = soma;
+
+// const NASCIMENTO = "07/09/1990";
+// let nome = "Gleidson Teixeira";
+// nome = "Anaady";
+
+// container.innerHTML = nome + NASCIMENTO;
+
+function somar()
+{
+    let v1 = Number(valor1.value);
+    let v2 = Number(valor2.value);
+    document.getElementById("resultado").innerHTML = v1 + v2;
+}
+
